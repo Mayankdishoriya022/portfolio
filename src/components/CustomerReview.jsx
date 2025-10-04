a1507acd726cae34d2bbeea65b8e88bfbfaeea5d
Mayank <mayankdishoriya022@gmail.com>
2025-10-04 13:00:04 +0530

    small changes

--- src/components/CustomerReview.jsx
+++ src/components/CustomerReview.jsx
@@ -8,20 +8,20 @@ const CustomerReview = () => {
     <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
       <div class="mx-auto flex max-w-xs flex-col gap-y-4">
         <dt class="text-base/7 text-gray-400">Transactions every 24 hours</dt>
-        <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">44 million</dd>
+        <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">4 million</dd>
       </div>
       <div class="mx-auto flex max-w-xs flex-col gap-y-4">
         <dt class="text-base/7 text-gray-400">Assets under holding</dt>
-        <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">$119 trillion</dd>
+        <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">$1 trillion</dd>
       </div>
       <div class="mx-auto flex max-w-xs flex-col gap-y-4">
         <dt class="text-base/7 text-gray-400">New users annually</dt>
-        <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">46,000</dd>
+        <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">4000</dd>
       </div>
     </dl>
   </div>
 </div>
-
+{/*  */}
     </div>
   )
 }
