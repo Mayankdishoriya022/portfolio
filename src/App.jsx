5b7eebca419e118af8c6265d89b1c0a74a471ca9
Mayank <mayankdishoriya022@gmail.com>
2025-10-04 13:03:40 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Banner from './components/Banner'
 import CustomerReview from './components/CustomerReview'
 import Footer from './components/Footer'
-import Header from './components/header'
+import Header from './components/Header'
 
 
 function App() {
